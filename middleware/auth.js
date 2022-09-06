630302a7705b95d9d682f66aa235a33a6445e380
Thomas Perrault <thomas.perrault.pro@gmail.com>
2022-09-06 23:11:11 +0200

    Debug middleware auth (oublie du 'next')

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,8 @@ module.exports = (req, res, next) => {
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
-        }
+        };
+    next();
     } 
     catch(error) {
         res.status(401).json({ error });
