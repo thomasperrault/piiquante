5ef42ff7355df5efe6a43d6a0fdf1fe2cb5f18e0
Thomas Perrault <thomas.perrault.pro@gmail.com>
2022-09-06 23:08:03 +0200

    Modification suite création éléments sauce (models, route, controler) et auth

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express'); //Importation de express 
 const mongoose = require('mongoose'); //Importation de mongoose
+const path = require('path'); //Accès au path du serveur
 
-const User = require('./models/User');
+//const User = require('./models/User'); //Importation du modele "user"
+//const Sauce = require('./models/Sauce'); //Importation du modele "sauce"
 
 const userRoutes = require('./routes/user'); //Importation du routeur "user" 
+const sauceRoutes = require('./routes/sauce'); //Importation du routeur "sauce"
 
 //Connexion à mongodb
 mongoose.connect('mongodb+srv://piiquanteUser:piiquanteUserMdp@piiquante.8mgit4j.mongodb.net/?retryWrites=true&w=majority',
@@ -28,6 +31,8 @@ app.use((req, res, next) => {
 
 //Routes
 app.use('/api/auth', userRoutes);    
+app.use('/api/sauces', sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 //Export de l'application pour y accéder depuis les autres fichiers du projet (serveur)
 module.exports = app;
